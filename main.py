4848b93af25d2d969fd4a0efb4f57d37d3bee42c
Muhammadfahril585 <laodefahril00@gmail.com>
2025-03-23 23:44:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -52,4 +52,6 @@ async def main():
 
 # Jalankan aplikasi dengan asyncio
 if __name__ == "__main__":
-    asyncio.run(main())
+    import asyncio
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
